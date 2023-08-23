bc0d5c61709b4ae9211dc29d96822bb5548e964b
rafacoelhog <142436648+rafacoelhog@users.noreply.github.com>
2023-08-23 17:04:09 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,99 @@
+et a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+
+//idade
+let minhaIdade = 15;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+
+/*
+//Exercicio 1 
+let f = 3;
+console.log("Antes", f = 3);
+f = f + 4;
+console.log("Depois", f);
+
+let g = 3;
+console.log("Antes", g = 3);
+g = g * 5;
+console.log("Depois", g);
+
+let h = 15;
+console.log("Antes",  h = 15);
+h = h / 2;
+console.log("Depois", h);
+
+let i = 5;
+console.log("Antes", i = 5);
+i = i - 4;
+console.log("Depois", i);
+let j = 234;
+let k = 5;
+console.log(j % k);
+*/
+
+/*
+//Exercicio 2
+let variavel1 = 5;
+let variavel2 = 7;
+console.log(variavel1 === variavel2);
+console.log(variavel1 !== variavel2);
+console.log(variavel1 > variavel2);
+console.log(variavel1 < variavel2);
+*/
+
+/*
+//Comparadores Logicos
+let A = 5;
+let B = 3;
+let C = 0;
+
+console.log(A === B || A !== C);
+console.log(A === B && A !== C);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+
+/*
+//Exercicio 3
+let A = true;
+let B = false;
+let C = true;
+
+console.log(A && B);
+console.log(B && C);
+console.log(A && C);
+console.log(A && B && C);
+
+console.log("---- separador ----");
+
+//Exercicio 4
+console.log(A || B);
+console.log(B || C);
+console.log(A || C);
+console.log(A || B || C);
+*/
+
+//Negação 
+let A = true; 
+let B = false;
+
+console.log(A);
+console.log(!A);
+
+console.log(B);
+console.log(!B);
+
